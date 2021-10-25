cea796e1c9c8cc00bae25f82e76dbc5bdc604fe8
HenriqueHartmann <alemaohartmann@gmail.com>
2021-10-25 15:58:21 -0300

    Add genre view

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genre/', views.GenreView.as_view()),
+    path('genre/<int:id>', views.GenreView.as_view()),
 ]
